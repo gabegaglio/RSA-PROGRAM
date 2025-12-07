ef85db9d27c97046d41eb52604ddbe5da13fe81b
Your Name <your.email@example.com>
2025-12-06 23:32:17 -0500

    RSA-Class: Move contents to root, remove other projects

--- RSA-Personal/quotrem/quotrem.h
+++
@@ -1,9 +0,0 @@
-#ifndef QUOTREM_H
-#define QUOTREM_H
-
-#include <utility>
-
-// Division algorithm - returns {quotient, remainder}
-std::pair<long long, long long> divisionAlgorithm(long long a, long long b);
-
-#endif
